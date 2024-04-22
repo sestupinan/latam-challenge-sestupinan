d05af4d484910d8cd4b43e966a286b9681d1f21d
sestupinan <s.estupinan@uniandes.edu.co>
2024-04-22 14:32:27 -0500

    Improved documentation

--- src/q3_time.py
+++ src/q3_time.py
@@ -2,16 +2,25 @@ from typing import List, Tuple
 import json
 
 def q3_time(file_path: str) -> List[Tuple[str, int]]:
+    
+    # Dictionary to store the count of each mentioned user
     mentioned_users_dict = {}
     with open(file_path) as f:
             for jsonObj in f:
                 tweetDict = json.loads(jsonObj)
+                # We only need the mentioned users JSON part
+                # ASSUMPTION: it is assumed that the mentioned users inside the quotedTweet part should not be counted for this
+                # (In case that it is needed, we only need to add another for statement that will behave similarly to the one below)
                 mentioned_users = tweetDict['mentionedUsers']
                 if mentioned_users is not None:
                     for user in mentioned_users:
                         username = user['username']
+                        
+                        # Sums 1 to the quantity of appereances of the emoji if it already exists
                         if mentioned_users_dict.get(username) is not None:
                             mentioned_users_dict[username] = mentioned_users_dict[username] + 1
+                            
+                        # Adds the emoji to the dictionary and sets the value to 1 if it did not exist
                         else:
                             mentioned_users_dict[username] = 1
 
