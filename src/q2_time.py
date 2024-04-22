737d3ecf6a80f238d237188161316c98cae1fa9a
sestupinan <s.estupinan@uniandes.edu.co>
2024-04-22 13:58:17 -0500

    Improved documentation

--- src/q2_time.py
+++ src/q2_time.py
@@ -4,23 +4,27 @@ import json
 
 def q2_time(file_path: str) -> List[Tuple[str, int]]:
     
+    # Regex pattern for emojis
     emoji_pattern = re.compile(r'[\U0001F300-\U0001F6FF\U0001F900-\U0001F9FF\u2600-\u26FF\u2700-\u27BF]', flags=re.UNICODE)
 
     emojis_dict = {}
     with open(file_path) as f:
             for jsonObj in f:
                 tweetDict = json.loads(jsonObj)
+                # Uses the regex previously created to find all the emojis in the tweet content
                 emojis = emoji_pattern.findall(tweetDict['content'])
                 for emoji in emojis:
+                    # Sums 1 to the quantity of appereances of the emoji if it already exists
                     if emojis_dict.get(emoji) is not None:
                         emojis_dict[emoji] = emojis_dict[emoji] + 1
+                    # Adds the emoji to the dictionary and sets the value to 1 if it did not exist
                     else:
                         emojis_dict[emoji] = 1
 
 
-    # Sort the dictionary by values in descending order
+    # Sorts the dictionary by values in descending order
     emojis_dict = sorted(emojis_dict.items(), key=lambda x: x[1], reverse=True)
 
-    # Extract the top 10 values
+    # Extracts the top 10 emojis
     emojis_dict = emojis_dict[:10]
     return emojis_dict
