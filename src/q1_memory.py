2b221597855398e02b1416a9dd06dadf6d92def5
Santiago Estupiñan <47406477+sestupinan@users.noreply.github.com>
2024-04-22 22:59:59 -0500

    Final version latam challenge (#4)
    * Feature/first problem finished code (#1)
    
    * Version 1 of the time-optimized function
    
    * added pandas to required packages
    
    * version 1 of time and memory measuring
    
    * Version 1 of the memory optimized function
    
    * added profiler
    
    * Improved analysis of time and memory
    
    * Feature/second problem (#2)
    
    * Version 1 of the time-optimized function
    
    * added pandas to required packages
    
    * Version 1 of time optimization
    
    * Improved documentation
    
    * Version 2 of methods and finished analysis
    
    * Feature/third problem finished (#3)
    
    * Version 1 of the time-optimized function
    
    * added pandas to required packages
    
    * Version 1 of time optimization
    
    * Improved documentation
    
    * Time optimized first version
    
    * Improved documentation
    
    * Fnished analysis for challenge 3
    
    * Fixes on notebook

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -1,5 +1,43 @@
 from typing import List, Tuple
 from datetime import datetime
+import json
+from datetime import datetime
+from collections import defaultdict
+from memory_profiler import profile
 
+# Function to calculate the username with most posts in each of the top 10 days with the most posts (optimizing memory)
+@profile
 def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+    # Dictionaries to store the information
+    date_list = defaultdict(int) # Dict with number of posts on that day
+    user_date_list = defaultdict(int) # Dict with number of posts per user on that day
+    
+    # Date format to use
+    format = '%Y-%m-%d'
+    with open(file_path) as f:
+        for jsonObj in f:
+            # To optimize memory, we are only storing in permanent variables the two fields we need (date and username)
+            tweetDict = json.loads(jsonObj)
+            formatted_date = datetime.strptime(tweetDict['date'].split('T')[0], format).date()
+            username = tweetDict['user']['username']
+            date_list[formatted_date] +=1
+            user_date_list[(formatted_date, username)]+=1
+
+    # Sorts the dictionary by values in descending order
+    date_list = sorted(date_list.items(), key=lambda x: x[1], reverse=True)
+
+    # Extracts the top 10 dates
+    date_list = date_list[:10]
+            
+    listed = []
+    
+    # Computes top user per day
+    for date, n in date_list:
+        max_tweets = 0
+        max_user = ''
+        for (tweet_date, username), count in user_date_list.items():        
+            if tweet_date == date and count > max_tweets:  
+                max_tweets = count
+                max_user = username
+        listed.append((date, max_user))
+    return listed
--- src/q2_time.py
+++ src/q2_time.py
@@ -1,4 +1,30 @@
 from typing import List, Tuple
+import re
+import json
+from memory_profiler import profile
+from collections import Counter
 
+
+@profile
 def q2_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+        
+    # Regex pattern for emojis
+    emoji_pattern = re.compile(r'[\U0001F300-\U0001F6FF\U0001F900-\U0001F9FF\u2600-\u26FF\u2700-\u27BF]', flags=re.UNICODE)
+
+    emojis_dict = Counter()
+    with open(file_path) as f:
+            for jsonObj in f:
+                tweetDict = json.loads(jsonObj)
+                # Uses the regex previously created to find all the emojis in the tweet content
+                emojis = emoji_pattern.findall(tweetDict['content'])
+                for emoji in emojis:
+                    # Sums 1 to the quantity of appereances of the emoji if it already exists
+                    if emojis_dict.get(emoji) is not None:
+                        emojis_dict[emoji] = emojis_dict[emoji] + 1
+                    # Adds the emoji to the dictionary and sets the value to 1 if it did not exist
+                    else:
+                        emojis_dict[emoji] = 1
+
+
+    # Sorts the dictionary by values in descending order
+    return emojis_dict.most_common(10)
